e0e7d43e4364dbd4257c9c72b5ff9b373d473f7c
gnawre31 <erwangli2@gmail.com>
2023-04-24 17:06:27 -0400

    implemented ability to drag, move, and drop pieces on the board

--- client/src/components/Tile.jsx
+++ client/src/components/Tile.jsx
@@ -1,17 +1,34 @@
-import PropTypes from 'prop-types';
-import { getPieceSVG } from './svg';
+import PropTypes from "prop-types";
+import { getPieceSVG } from "./svg";
+import { dropPiece, grabPiece } from "./pieceDragAndDrop";
 
+const Tile = ({ tile, chessboardRef, activePiece, setActivePiece }) => {
 
-const Tile = ({ tile }) => {
-    const tileColor = (tile.x + tile.y) % 2 === 0 ? "black-tile" : "white-tile"
-    const piece = tile.isOccupied ? tile.pieceColor + "_" + tile.piece : null
-    const pieceSVG = piece ? getPieceSVG(piece) : null
+    const onClick = (e) => {
+        if (activePiece == null) {
+            setActivePiece(e.target);
+            grabPiece(e, chessboardRef.current);
+        } else {
+            dropPiece(e, chessboardRef.current);
+            setActivePiece(null);
+        }
+    };
+
+    const tileColor = (tile.x + tile.y) % 2 === 0 ? "black-tile" : "white-tile";
+    const piece = tile.isOccupied ? tile.pieceColor + "_" + tile.piece : null;
+    const pieceSVG = piece ? getPieceSVG(piece) : null;
     return (
-        <div className={"tile " + tileColor} >
-            {pieceSVG && <div style={{ backgroundImage: `url(${pieceSVG})` }} className='piece' />}
+        <div className={"tile " + tileColor} ref={chessboardRef}>
+            {pieceSVG && (
+                <div
+                    style={{ backgroundImage: `url(${pieceSVG})` }}
+                    className="piece"
+                    onClick={(e) => onClick(e)}
+                />
+            )}
         </div>
-    )
-}
+    );
+};
 
 Tile.propTypes = {
     tile: PropTypes.shape({
@@ -20,8 +37,13 @@ Tile.propTypes = {
         pos: PropTypes.string.isRequired,
         piece: PropTypes.string,
         pieceColor: PropTypes.string,
-        isOccupied: PropTypes.bool.isRequired
-    })
-}
+        isOccupied: PropTypes.bool.isRequired,
+    }),
+    chessboardRef: PropTypes.shape({
+        current: PropTypes.object,
+    }),
+    activePiece: PropTypes.object,
+    setActivePiece: PropTypes.func
+};
 
-export default Tile
+export default Tile;
