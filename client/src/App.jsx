cb609b6513f1decfacfaea7f732fc8de99e7dfe4
gnawre31 <erwangli2@gmail.com>
2023-04-26 17:04:34 -0400

    move pieces

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import { useChessStore } from './store'
 
 function App() {
   const newGame = useChessStore(state => state.newGame)
+  const board = useChessStore(state => state.board)
   const startGame = () => {
     const game = {
-      roomId: 123, gameId: 456, playerId: 789, playerColor: 'WHITE', opponentId: 1
+      roomId: 123, gameId: 456, playerId: 789, playerColor: 'WHITE', opponentId: 1, board: board
     }
+    // clearBoard()
     newGame(game)
   }
 
